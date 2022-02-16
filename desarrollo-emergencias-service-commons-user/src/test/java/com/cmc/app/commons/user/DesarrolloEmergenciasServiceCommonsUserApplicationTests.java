8a3ec6c3d9353c3f3d11377af0a85c2aa87ba991
Efrain Restrepo <webmaster@webcol.net>
2022-02-16 14:19:01 -0500

    Se carga microservicio Commons

---
+++ desarrollo-emergencias-service-commons-user/src/test/java/com/cmc/app/commons/user/DesarrolloEmergenciasServiceCommonsUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cmc.app.commons.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesarrolloEmergenciasServiceCommonsUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
