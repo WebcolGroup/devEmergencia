df824779522f2b0252fc45bbbba92b3da88dd1bc
Efrain Restrepo <webmaster@webcol.net>
2022-02-16 08:50:48 -0500

    Iniciamos el proyecto, creamos la entidad User y Rol

---
+++ desarrollo-emergencias-service-user/src/main/java/com/cmc/app/user/DesarrolloEmergenciasServiceUserApplication.java
@@ -0,0 +1,13 @@
+package com.cmc.app.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesarrolloEmergenciasServiceUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesarrolloEmergenciasServiceUserApplication.class, args);
+	}
+
+}
---
+++ desarrollo-emergencias-service-user/src/test/java/com/cmc/app/user/DesarrolloEmergenciasServiceUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cmc.app.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesarrolloEmergenciasServiceUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
