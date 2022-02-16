190644188bff56e8751d7e99fecb5cfb578126b0
Efrain Restrepo <webmaster@webcol.net>
2022-02-16 09:09:50 -0500

    Se carga modificacion en user para muchos a uno y se crea el properties

--- desarrollo-emergencias-service-user/src/main/java/com/cmc/app/user/entity/User.java
+++ desarrollo-emergencias-service-user/src/main/java/com/cmc/app/user/entity/User.java
@@ -2,15 +2,19 @@ package com.cmc.app.user.entity;
 
 import java.io.Serializable;
 import java.util.Date;
+import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+
+import org.hibernate.annotations.ManyToAny;
 
 @Entity
 @Table(name = "tbluser")
@@ -27,6 +31,17 @@ public class User implements Serializable {
 	@Temporal(TemporalType.TIMESTAMP)
 	private Date createAt;
 	private Boolean enabled;
+
+	@ManyToAny(fetch = FetchType.LAZY, metaColumn = @Column)
+	private List<Rol> roles;
+
+	public List<Rol> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(List<Rol> roles) {
+		this.roles = roles;
+	}
 
 	public Long getId() {
 		return id;
